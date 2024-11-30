074437bdaaf085489bb995cbdab3950ad15a7c46
Aldas <brutall100@gmail.com>
2024-11-30 15:10:26 +0200

    Used context on stories page

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import CategoryPage from "./pages/CategoryPage/CategoryPage";
-import MealPage from "./pages/MealPage/MealPage"
-import AllMealsPage from "./pages/AllMealsPage/AllMealsPage"
-import StoriesPage from "./pages/StoriesPage/StoriesPage"
+import MealPage from "./pages/MealPage/MealPage";
+import AllMealsPage from "./pages/AllMealsPage/AllMealsPage";
+import StoriesPage from "./pages/StoriesPage/StoriesPage";
+import { StoriesProvider } from "./context/StoriesContext"; // Import StoriesProvider
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<ErrorPage />} />
-        <Route path="/stories" element={<StoriesPage />} />
-        <Route path="/categories/:id" element={<CategoryPage />} />
-        <Route path="/meals" element={<AllMealsPage />} />
-        <Route path="/meals/:id" element={<MealPage />} />
-      </Routes>
+      <StoriesProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<ErrorPage />} />
+          <Route path="/stories" element={<StoriesPage />} />
+          <Route path="/categories/:id" element={<CategoryPage />} />
+          <Route path="/meals" element={<AllMealsPage />} />
+          <Route path="/meals/:id" element={<MealPage />} />
+        </Routes>
+      </StoriesProvider>
     </>
   );
 }
